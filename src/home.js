e6708290fccc5db329b7d84d1263432ebdcbc93f
Sahira100 <e19100@eng.pdn.ac.lk>
2024-06-16 13:41:03 +0530

    final 1v

---
+++ src/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function home() {
+  const style = { color: "red", fontSize: "12px" };
+
+  return <div style={style}>home</div>;
+}
+
+export default home;
